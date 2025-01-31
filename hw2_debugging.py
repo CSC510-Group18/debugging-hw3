d08f7753fffd9ef6f8c4cb9ff828fe46ab533069
lnotspotl <lnotspotl@gmail.com>
2025-01-31 16:54:59 -0500

    First hw03 half

--- hw2_debugging.py
+++ hw2_debugging.py
@@ -1,36 +1,71 @@
-import rand
+"""
+Homework 2 Debugging
+"""
 
-def mergeSort(arr):
-    if (len(arr) == 1):
+import random
+
+
+def merge_sort(arr):
+    """Sort an array using merge sort"""
+    if len(arr) == 1:
         return arr
 
-    half = len(arr)//2
+    half = len(arr) // 2
 
-    return recombine(mergeSort(arr[:half]), mergeSort(arr[half:]))
+    return recombine(merge_sort(arr[:half]), merge_sort(arr[half:]))
 
-def recombine(leftArr, rightArr):
-    leftIndex = 0
-    rightIndex = 0
-    mergeArr = [None] * (len(leftArr) + len(rightArr))
-    while leftIndex < len(leftArr) and rightIndex < len(rightArr):
-        if leftArr[leftIndex] < rightArr[rightIndex]:
-            rightIndex += 1
-            mergeArr[leftIndex + rightIndex] = leftArr[leftIndex]
+
+def recombine(left_array, right_array):
+    """Recombine two sorted arrays into a single sorted array"""
+    left_index = 0
+    right_index = 0
+    merge_index = 0
+    merge_arr = [None] * (len(left_array) + len(right_array))
+
+    while left_index < len(left_array) and right_index < len(right_array):
+        if left_array[left_index] < right_array[right_index]:
+            merge_arr[merge_index] = left_array[left_index]
+            left_index += 1
         else:
-            leftIndex += 1
-            mergeArr[leftIndex + rightIndex] = rightArr[rightIndex]
+            merge_arr[merge_index] = right_array[right_index]
+            right_index += 1
+        merge_index += 1
 
-    for i in range(rightIndex, len(rightArr)):
-        mergeArr[leftIndex + rightIndex] = rightArr[i]
-    
-    for i in range(leftIndex, len(leftArr)):
-        mergeArr[leftIndex + rightIndex] = leftArr[i]
+    while right_index < len(right_array):
+        merge_arr[merge_index] = right_array[right_index]
+        right_index += 1
+        merge_index += 1
 
-    return mergeArr
+    while left_index < len(left_array):
+        merge_arr[merge_index] = left_array[left_index]
+        left_index += 1
+        merge_index += 1
 
-arr = rand.random_array([None] * 20)
-arr_out = mergeSort(arr)
-
-print(arr_out)
+    return merge_arr
 
 
+def linear_search(arr, item):
+    """
+    Finds the index of an item in the array and return this index.
+    If not found, this function returns None
+    """
+
+    for i, value in enumerate(arr):
+        if value == item:
+            return i
+    return None
+
+
+if __name__ == "__main__":
+    print("Testing...")
+
+    sample_array = [random.randint(0, 100) for _ in range(20)]
+
+    assert sorted(sample_array) == merge_sort(sample_array), "Merge sort is not working"
+
+    idx = random.randint(0, len(sample_array) - 1)
+    item = sample_array[idx]
+
+    assert idx == linear_search(sample_array, item), "Linear search is not working"
+
+    print("All tests passed!")
