37f03ba83f33833b7d2f6cadd07a257f81bf4890
zeiadyakout <120664949+zeiadyakout@users.noreply.github.com>
2025-02-03 00:51:22 +0000

    ail pylint's despair

--- bogosort.py
+++ bogosort.py
@@ -4,35 +4,34 @@ Citation: https://www.geeksforgeeks.org/python-program-for-bogosort-or-permutati
 """
 import random
 
-"""
-Main method for BogoSort implementation
-"""
+
 def bogo_sort(a):
-    n = len(a)
-    while is_sorted(a)== False:
+    """
+    Main method for BogoSort implementation
+    """
+    while is_sorted(a) is False:
         shuffle(a)
 
-"""
-Checks if input array is sorted or not
-
-Inputs: a --> input array
-
-Outputs: True if array is sorted, False otherwise
-"""
 def is_sorted(a):
+    """
+    Checks if input array is sorted or not
+
+    Inputs: a --> input array
+
+    Outputs: True if array is sorted, False otherwise
+    """
     n = len(a)
     for i in range(0, n-1):
         if a[i] > a[i+1]:
             return False
     return True
 
-"""
-Generates a permutation of the array
-
-Inputs: a --> input array
-"""
-
 def shuffle(a):
+    """
+    Generates a permutation of the array
+
+    Inputs: a --> input array
+    """
     n = len(a)
     for i in range (0,n):
         r = random.randint(0,n-1)
