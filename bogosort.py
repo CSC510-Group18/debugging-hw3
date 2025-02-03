02f13edcdd44067ea0e4a23449936e3ae0a5d362
zeiadyakout <120664949+zeiadyakout@users.noreply.github.com>
2025-02-03 00:47:45 +0000

    Modified files to adhere to Actions guidelines

--- bogosort.py
+++ bogosort.py
@@ -4,20 +4,34 @@ Citation: https://www.geeksforgeeks.org/python-program-for-bogosort-or-permutati
 """
 import random
 
-def bogoSort(a):
+"""
+Main method for BogoSort implementation
+"""
+def bogo_sort(a):
     n = len(a)
-    while (is_sorted(a)== False):
+    while is_sorted(a)== False:
         shuffle(a)
- 
-# Checks if input array is sorted or not
+
+"""
+Checks if input array is sorted or not
+
+Inputs: a --> input array
+
+Outputs: True if array is sorted, False otherwise
+"""
 def is_sorted(a):
     n = len(a)
     for i in range(0, n-1):
-        if (a[i] > a[i+1] ):
+        if a[i] > a[i+1]:
             return False
     return True
- 
-# Generates a permutation of the array
+
+"""
+Generates a permutation of the array
+
+Inputs: a --> input array
+"""
+
 def shuffle(a):
     n = len(a)
     for i in range (0,n):
